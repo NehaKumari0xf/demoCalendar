0dd599ec753b983471c9f6c47d57bb253bedeee5
Neha Kumari <158999099+NehaKumari0xf@users.noreply.github.com>
2024-05-05 11:51:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const calendarBody = document.getElementById('calendar-body');
+
+    // Fill in the calendar with days
+    let date = 1;
+    for (let i = 0; i < 4; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j < 7; j++) {
+            const cell = document.createElement('td');
+            const cellContent = document.createElement('div');
+            cellContent.textContent = date;
+            cell.appendChild(cellContent);
+            row.appendChild(cell);
+            date++;
+        }
+        calendarBody.appendChild(row);
+    }
+});
